d87ca3c8a7652a852301290acdba5c2a91cebc04
rbruhn <bruhnrp@gmail.com>
2026-02-14 14:11:35 -0500

    Refactor navbar for improved semantic structure and accessibility, and modularize SCSS
    - Updated mobile hamburger toggler to use `<button>` with better ARIA support and focusability.
    - Enhanced dropdown accessibility with improved hover and focus states.
    - Modularized SCSS structure by splitting styles into focused partials for components, layout, and pages.
    - Added `_dropdowns.scss` and `_home.scss` partials for better maintainability.
    - Removed unused global styles from `main.scss`.

--- resources/views/common/nav.blade.php
+++ resources/views/common/nav.blade.php
@@ -1,13 +1,21 @@
-<div class="three col d-block d-sm-block d-md-none" data-toggle="collapse" data-target="#navbarsExampleDefault"
-     aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-    <div class="hamburger" id="hamburger-9">
+{{-- Mobile hamburger toggler (must be focusable for keyboard/ADA) --}}
+<button
+        type="button"
+        class="navbar-toggler d-block d-sm-block d-md-none ml-auto p-0 border-0 bg-transparent"
+        data-toggle="collapse"
+        data-target="#navbarsExampleDefault"
+        aria-controls="navbarsExampleDefault"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+>
+    <span class="hamburger" id="hamburger-9" aria-hidden="true">
         <span class="line"></span>
         <span class="line"></span>
         <span class="line"></span>
-    </div>
-</div>
+    </span>
+</button>
 
-<div class="collapse navbar-collapse" id="navbarsExampleDefault">
+<div class="collapse navbar-collapse w-100" id="navbarsExampleDefault">
     <ul class="navbar-nav ml-auto">
         <li class="nav-item mr-2 dropdown">
             <a class="nav-link dropdown-toggle text-uppercase" href="#" id="dropdown00" data-toggle="dropdown"
@@ -20,13 +28,17 @@
                    href="{{ route('front.site-assets.index') }}">{{ t('Resources') }}</a>
             </div>
         </li>
+
         <li class="nav-item active">
-            <a class="nav-link mr-2 text-uppercase" href="{{ route('front.projects.index') }}">{{ t('Projects') }} <span
-                        class="sr-only">(current)</span></a>
+            <a class="nav-link mr-2 text-uppercase" href="{{ route('front.projects.index') }}">
+                {{ t('Projects') }} <span class="sr-only">(current)</span>
+            </a>
         </li>
+
         <li class="nav-item mr-2">
             <a class="nav-link text-uppercase" href="{{ route('front.expeditions.index') }}">{{ t('Expeditions') }}</a>
         </li>
+
         <li class="nav-item mr-2">
             <a class="nav-link text-uppercase" href="{{ route('front.events.index') }}">{{ t('Events') }}</a>
         </li>
@@ -43,6 +55,7 @@
                        data-href="{{ route('front.wedigbio-progress', [$event]) }}"
                        data-channel="{{ config('config.poll_wedigbio_progress_channel') . '.' . $event->uuid }}"
                        data-uuid="{{ $event->uuid }}">{{ t('Progress') }}</a>
+
                     <a class="dropdown-item text-uppercase" href="#"
                        data-toggle="modal"
                        data-target="#wedigbio-rate-modal"
@@ -50,24 +63,29 @@
                        data-uuid="{{ $event->uuid }}"
                        data-href="{{ route('front.get.wedigbio-rate', [$event]) }}">{{ t('Rates') }}</a>
                 @endisset
+
                 <a class="dropdown-item text-uppercase"
                    href="{{ route('front.wedigbio.index') }}">{{ t('Past Events') }}</a>
+
                 <a class="dropdown-item text-uppercase" href="https://wedigbio.org"
-                   target="_blank">{{ t('WeDigBio Website') }}</a>
+                   target="_blank" rel="noopener">{{ t('WeDigBio Website') }}</a>
             </div>
         </li>
 
         <li class="nav-item mr-2">
             <a class="nav-link text-uppercase" href="{{ route('front.bingos.index') }}">{{ t('Games') }}</a>
         </li>
+
         <li class="nav-item mr-2">
             <a class="nav-link text-uppercase" href="{{ route('front.contact.index') }}">{{ t('Contact') }}</a>
         </li>
+
         @if(Auth::check())
             <li class="nav-item nav-btn dropdown">
                 <a class="nav-link dropdown-toggle text-uppercase" href="#" id="dropdown02" data-toggle="dropdown"
                    aria-haspopup="true"
                    aria-expanded="false">{{ t('Admin') }}</a>
+
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown02">
                     <a href="{{ route('admin.groups.index') }}" class="dropdown-item text-uppercase"
                        type="button">{{ t('Manage Groups') }}</a>
@@ -93,22 +111,25 @@
                         <a href="{{ route('admin.mail.index') }}" class="dropdown-item text-uppercase"
                            type="button">{{ t('Mail') }}</a>
                     @endcan
+
                     <a href="{{ route('admin.update.index') }}" class="dropdown-item text-uppercase"
                        type="button">{{ t('Updates') }}</a>
+
                     <a href="{{ route('admin.users.edit', [Auth::user()]) }}" class="dropdown-item text-uppercase"
                        type="button">{{ t('Account') }}</a>
+
                     <form method="POST" action="{{ route('app.post.logout') }}">
                         @csrf
                         <button type="submit" class="dropdown-item text-uppercase">
                             {{ t('Logout') }}
                         </button>
                     </form>
-                    </div>
-                </li>
-            @else
-                <li class="nav-item nav-btn">
-                    <a class="nav-link text-uppercase mx-auto" href="{{ route('app.get.login') }}">{{ t('Login') }}</a>
-                </li>
-            @endif
+                </div>
+            </li>
+        @else
+            <li class="nav-item nav-btn">
+                <a class="nav-link text-uppercase mx-auto" href="{{ route('app.get.login') }}">{{ t('Login') }}</a>
+            </li>
+        @endif
     </ul>
 </div>

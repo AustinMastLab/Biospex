baba094e75446f60b206c8bdfd39ad0b6fce38d5
rbruhn <bruhnrp@gmail.com>
2026-03-09 21:27:26 -0400

    Refine text alignment and remove redundant `text-justify` classes in `home.blade.php` and `default.blade.php` for improved readability.

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -37,7 +37,7 @@
     <section class="home-heading text-center" id="learn-more">
         <div class="container">
             <img src="/images/page/logo-tagline-action.png" alt="BIOSPEX: Lead Public Digitization Expeditions">
-            <p class="text-justify mt-4">{{ t('BIOSPEX is a base camp for launching, advertising, and managing targeted
+            <p class="mt-4">{{ t('BIOSPEX is a base camp for launching, advertising, and managing targeted
             efforts to digitize the world\'s 3 billion biodiversity research specimens in ways that involve the public.
             Such specimens include fish in jars, plants on sheets, fossils in drawers, insects on pins, and many other
             types. “Digitization” is a broad reference to creating digital data about the physical specimens and includes
